8754b711a343e642fa68a91df2cda738de5a0a87
Gabriel Silva <gabrielrsilva11@gmail.com>
2023-11-17 17:06:18 +0000

    Code Updates

--- PyTorchConvert.py
+++ PyTorchConvert.py
@@ -11,72 +11,41 @@ import torch.nn.functional as F
 import tqdm
 
 
-def fetch_graph(random_list):
+def fetch_graph(random_list, nodes_uri, edges_uri):
     print("--- LOADING DATASET ---")
-    base_uri = "http://ieeta-bit.pt/wikiner#"
-    graph_name = "WikiNER"
-    conection_string = 'http://estga-fiware.ua.pt:8890/sparql'
-    ntx = BuildNetworkx(base_uri, graph_name, conection_string)
-    graph = ntx.fetch_graph(random_list)
-    number_of_words = 0
-    le = preprocessing.LabelEncoder()
-    for s, p, o in graph.triples((None, RDF.type, URIRef("http://ieeta-bit.pt/wikiner#Word"))):
-        number_of_words += 1
-        entity = ''
-        for s2, p2, o2 in graph.triples((URIRef(s), URIRef("http://ieeta-bit.pt/wikiner#wikinerEntity"), None)):
-            entity = o2
-        if entity:
-            entity_no_break = entity.split("\n")
-            entity_correct = entity_no_break[0].split("-")
-            graph.remove((s2, p2, o2))
-            graph.add((URIRef(s), URIRef("http://ieeta-bit.pt/wikiner#wikinerEntity"), Literal(entity_correct[1])))
-        else:
-            graph.add((URIRef(s), URIRef("http://ieeta-bit.pt/wikiner#wikinerEntity"), Literal("No")))
-
-    return graph
-
-
-def build_node_relationships(uniqueIds, nodeList):
-    original_df = pd.DataFrame(data=nodeList, columns=["source", "target"])
-    source_df = pd.merge(original_df['source'], uniqueIds,
-                                  left_on='source', right_on='originalId', how='left')
-    source = torch.from_numpy(source_df['mappedId'].values)
-    target_df = pd.merge(original_df['target'], uniqueIds,
-                                  left_on='target', right_on='originalId', how='left')
-    target = torch.from_numpy((target_df['mappedId'].values))
-    node_tensor = torch.stack([source, target], dim=0)
-    return node_tensor
-
-
-def fetch_graph_info(graph, nodes_uri, edges_uri):
-    edge_df = pd.DataFrame()
     index_list = []
     nodes_list = []
     depgraph_list = []
     head_list = []
     prev_list = []
-    for s, p, o in graph.triples((None, RDF.type, URIRef(nodes_uri))):
-        node_features = {}
-        #Index to build the node/edges dataframe.
-        #In the edges this index will be the "source" node
-        index_list.append(s.__str__())
-        for s2, p2, o2 in graph.triples((URIRef(s), None, None)):
-            #Add the edges information
-            if p2.__str__() in edges_uri:
-                if p2.__str__() == depgraph_uri:
-                    depgraph_list.append([s2.__str__(), o2.__str__()])
-                elif p2.__str__() == head_uri:
-                    head_list.append([s2.__str__(), o2.__str__()])
-                elif p2.__str__() == previous_uri:
-                    prev_list.append([s2.__str__(), o2.__str__()])
-
-            #Add the node features
+    base_uri = "http://ieeta-bit.pt/wikiner#"
+    graph_name = "WikiNER"
+    conection_string = 'http://estga-fiware.ua.pt:8890/sparql'
+    ntx = BuildNetworkx(base_uri, graph_name, conection_string)
+    add = 0
+    for idx in random_list:
+        graph = ntx.fetch_graph([idx])
+        # number_of_words = 0
+        for s, p, o in graph.triples((None, RDF.type, URIRef("http://ieeta-bit.pt/wikiner#Word"))):
+            # number_of_words += 1
+            entity = ''
+            for s2, p2, o2 in graph.triples((URIRef(s), URIRef("http://ieeta-bit.pt/wikiner#wikinerEntity"), None)):
+                entity = o2
+            if entity:
+                entity_no_break = entity.split("\n")
+                entity_correct = entity_no_break[0].split("-")
+                graph.remove((s2, p2, o2))
+                graph.add((URIRef(s), URIRef("http://ieeta-bit.pt/wikiner#wikinerEntity"), Literal(entity_correct[1])))
             else:
-                node_feat_split = p2.__str__().split("#")
-                feat_name = node_feat_split[-1]
-                node_features[feat_name] = o2.__str__()
-        nodes_list.append(node_features)
+                add += 1
+                if add == 20:
+                    graph.add((URIRef(s), URIRef("http://ieeta-bit.pt/wikiner#wikinerEntity"), Literal("No")))
+                    add = 0
 
+        depgraph_list, head_list, prev_list, nodes_list, index_list = fetch_graph_info(graph, nodes_uri, edges_uri,
+                                                                                       depgraph_list, head_list,
+                                                                                       prev_list, nodes_list,
+                                                                                       index_list)
     #map word URI to consecutive values
     unique_ids_df = pd.DataFrame(data={
         'originalId': index_list,
@@ -113,6 +82,85 @@ def fetch_graph_info(graph, nodes_uri, edges_uri):
     nodes_df = nodes_df.drop(['wikinerEntity'], axis=1)
     nodes_tensor = torch.from_numpy(nodes_df.values).to(torch.float)
     return nodes_tensor, depgraph_graph, head_graph, previous_graph, targets, unique_ids_df, unique_targets_df
+    #return graph
+
+
+def build_node_relationships(uniqueIds, nodeList):
+    original_df = pd.DataFrame(data=nodeList, columns=["source", "target"])
+    source_df = pd.merge(original_df['source'], uniqueIds,
+                         left_on='source', right_on='originalId', how='left')
+    source = torch.from_numpy(source_df['mappedId'].values)
+    target_df = pd.merge(original_df['target'], uniqueIds,
+                         left_on='target', right_on='originalId', how='left')
+    target = torch.from_numpy((target_df['mappedId'].values))
+    node_tensor = torch.stack([source, target], dim=0)
+    return node_tensor
+
+
+def fetch_graph_info(graph, nodes_uri, edges_uri, depgraph_list, head_list, prev_list, nodes_list, index_list):
+    # index_list = []
+    # nodes_list = []
+    # depgraph_list = []
+    # head_list = []
+    # prev_list = []
+    for s, p, o in graph.triples((None, RDF.type, URIRef(nodes_uri))):
+        node_features = {}
+        # Index to build the node/edges dataframe.
+        # In the edges this index will be the "source" node
+        index_list.append(s.__str__())
+        for s2, p2, o2 in graph.triples((URIRef(s), None, None)):
+            # Add the edges information
+            if p2.__str__() in edges_uri:
+                if p2.__str__() == depgraph_uri:
+                    depgraph_list.append([s2.__str__(), o2.__str__()])
+                elif p2.__str__() == head_uri:
+                    head_list.append([s2.__str__(), o2.__str__()])
+                elif p2.__str__() == previous_uri:
+                    prev_list.append([s2.__str__(), o2.__str__()])
+
+            # Add the node features
+            else:
+                node_feat_split = p2.__str__().split("#")
+                feat_name = node_feat_split[-1]
+                node_features[feat_name] = o2.__str__()
+        nodes_list.append(node_features)
+    return depgraph_list, head_list, prev_list, nodes_list, index_list
+    # map word URI to consecutive values
+    # unique_ids_df = pd.DataFrame(data={
+    #     'originalId': index_list,
+    #     'mappedId': pd.RangeIndex(len(index_list))
+    # })
+    #
+    # #Build the previousWord relationship: word - previousWord - word
+    # previous_graph = build_node_relationships(unique_ids_df, prev_list)
+    # head_graph = build_node_relationships(unique_ids_df, head_list)
+    # depgraph_graph = build_node_relationships(unique_ids_df, depgraph_list)
+    #
+    # #Build dataframe with wordId -> features of each word
+    # nodes_df = pd.DataFrame.from_records(nodes_list, index=unique_ids_df['mappedId'])
+    # nodes_df.index.name = "wordId"
+    # feats = nodes_df['feats'].str.get_dummies('|')
+    # nodes_df = nodes_df.join(feats)
+    # nodes_df = nodes_df.drop(['feats', 'type', 'id'], axis=1)
+    # nodes_df.edge = nodes_df.edge.astype('category').cat.codes
+    # nodes_df.lemma = nodes_df.lemma.astype('category').cat.codes
+    # nodes_df.pos = nodes_df.pos.astype('category').cat.codes
+    # nodes_df.poscoarse = nodes_df.poscoarse.astype('category').cat.codes
+    # nodes_df.word = nodes_df.word.astype('category').cat.codes
+    #
+    # #handle the Y (targets)
+    # #targets = nodes_df.pop('wikinerEntity')
+    # unique_targets_id = nodes_df['wikinerEntity'].unique()
+    # unique_targets_df = pd.DataFrame(data={
+    #     'originalId': unique_targets_id,
+    #     'mappedId': pd.RangeIndex(len(unique_targets_id))
+    # })
+    # targets_df = pd.merge(nodes_df['wikinerEntity'], unique_targets_df,
+    #                               left_on='wikinerEntity', right_on='originalId', how='left')
+    # targets = torch.from_numpy(targets_df['mappedId'].values)
+    # nodes_df = nodes_df.drop(['wikinerEntity'], axis=1)
+    # nodes_tensor = torch.from_numpy(nodes_df.values).to(torch.float)
+    # return nodes_tensor, depgraph_graph, head_graph, previous_graph, targets, unique_ids_df, unique_targets_df
 
 
 class GAT(torch.nn.Module):
@@ -150,10 +198,12 @@ previous_uri = "http://ieeta-bit.pt/wikiner#previousWord"
 fromsentence_uri = "http://ieeta-bit.pt/wikiner#fromSentence"
 fromtext_uri = "http://ieeta-bit.pt/wikiner#fromText"
 
+# graph = fetch_graph([*range(1, 300, 1)])
+nodes, edged_graphs, head_graphs, prev_graphs, targets, mapped_ids, target_ids = fetch_graph([*range(1, 2000, 1)],
+                                                                                             nodes_uri=words_uri,
+                                                                                             edges_uri=[depgraph_uri, head_uri, previous_uri, fromsentence_uri])
 
-#graph = fetch_graph([*range(1, 300, 1)])
-graph = fetch_graph([*range(1, 300, 1)])
-nodes, edged_graphs, head_graphs, prev_graphs, targets, mapped_ids, target_ids = fetch_graph_info(graph, words_uri, [depgraph_uri, head_uri, previous_uri, fromsentence_uri])
+# nodes, edged_graphs, head_graphs, prev_graphs, targets, mapped_ids, target_ids = fetch_graph_info(graph, words_uri, [depgraph_uri, head_uri, previous_uri, fromsentence_uri])
 
 
 data = HeteroData()
@@ -174,9 +224,9 @@ data['word', 'previousWord', 'word'].edge_index = prev_graphs
 # data['sentence', '']
 
 
-#CREATING THE TRAIN, TEST AND VAL MASKS
+# CREATING THE TRAIN, TEST AND VAL MASKS
 print(data)
-split  = T.RandomNodeSplit(num_val=0.1, num_test=0.2)
+split = T.RandomNodeSplit(num_val=0.1, num_test=0.2)
 data_split = split(data)
 print(data_split)
 
@@ -184,7 +234,7 @@ model = GAT(hidden_channels=64, out_channels=data.num_classes)
 model = to_hetero(model, data.metadata(), aggr='sum')
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-#Pass data onto GPU
+# Pass data onto GPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Device: '{device}'")
 model = model.to(device)
@@ -203,3 +253,10 @@ for epoch in range(1, 50):
 # print(truth)
 print(data_split["word"].train_mask)
 print(data_split["word"].val_mask)
+preds = []
+out = model(data_split.x_dict, data_split.edge_index_dict)
+mask = data_split['word'].test_mask
+probabilities = torch.nn.functional.softmax(out['word'][mask], dim=0)
+truth = data_split['word'].y[mask]
+print(probabilities)
+print(truth)
